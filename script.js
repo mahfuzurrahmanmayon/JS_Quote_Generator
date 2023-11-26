02b5fa5fe10749e881f8e4593afeda6b4ae5f78c
Mahfuzur Rahman <mahfujurrahman291003@gmail.com>
2023-11-26 15:50:18 +0600

    JS Done

--- script.js
+++ script.js
@@ -1,5 +1,30 @@
-const api = "https://type.fit/api/quotes";
+const getNewQuote = async () => {
 
-fetch(api)
-    .then(res => res.json())
-    .then(data => console.log(data))
+    var url = "https://type.fit/api/quotes";
+
+    const response = await fetch(url);
+    const allQuote = await response.json();
+
+    const randomNumber = Math.floor(Math.random() * allQuote.length);
+    const randomQuote = allQuote[randomNumber]
+
+    console.log(randomQuote)
+
+    const quoteTextContainer = document.querySelector(".quote-text-container");
+    console.log(quoteTextContainer)
+    quoteTextContainer.innerHTML = `
+        <div class="quote-text">
+            ${randomQuote.text}
+        </div>
+        <div class="quote-author">
+            ${randomQuote.author}
+        </div>
+    `
+
+}
+
+const button = document.getElementById("get-quote");
+button.addEventListener("click", function () {
+    getNewQuote(); 
+});
+
